42d1d937d8e57b200b349137d6b20aa296ac7dc8
Zaid <mkhalidkachhawa@gmail.com>
2026-07-29 16:05:54 +0530

    changes

--- models/yunet_detector.py
+++ models/yunet_detector.py
@@ -1,3 +1,42 @@
+# import os
+# import cv2
+#
+# # =====================================
+# # Load YuNet Model
+# # =====================================
+#
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#
+# MODEL_PATH = os.path.join(
+#     BASE_DIR,
+#     "models",
+#     "face_detection_yunet_2023mar.onnx"
+# )
+#
+# detector = cv2.FaceDetectorYN.create(
+#     MODEL_PATH,
+#     "",
+#     (320, 320),
+#     score_threshold=0.8,
+#     nms_threshold=0.3,
+#     top_k=5000
+# )
+#
+#
+# def detect_faces(image):
+#     """
+#     Detect faces using YuNet.
+#     Returns:
+#         faces (numpy array) or None
+#     """
+#
+#     h, w = image.shape[:2]
+#
+#     detector.setInputSize((w, h))
+#
+#     _, faces = detector.detect(image)
+#
+#     return faces
 import os
 import cv2
 
@@ -17,7 +56,7 @@ detector = cv2.FaceDetectorYN.create(
     MODEL_PATH,
     "",
     (320, 320),
-    score_threshold=0.8,
+    score_threshold=0.6,
     nms_threshold=0.3,
     top_k=5000
 )
